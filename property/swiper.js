d32f27328695d18fd9d60ecc490ccf837e87d7e8
valclassdevelop <79193720+valclassdevelop@users.noreply.github.com>
2021-04-13 02:58:09 +0700

    Add files via upload

---
+++ property/swiper.js
@@ -0,0 +1,16 @@
+var swiper = new Swiper('.swiper-container', {
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: 'auto',
+    coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+    },
+    });
