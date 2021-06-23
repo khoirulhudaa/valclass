7a3a30e28a7a50cf1bc77f007249d50cb4b0f3b0
Muhammad Khoirul Huda <79193720+valclassdevelop@users.noreply.github.com>
2021-06-23 23:46:31 +0700

    Update valclass.js

--- valclass.js
+++ valclass.js
@@ -689,7 +689,17 @@ if(document.querySelector("body").classList.contains("image")) {
 }
 
 if(document.querySelector("body").classList.contains("3d")) {
-    js = "https://cdn.jsdelivr.net/gh/valclassdevelop/valclassDocumentation/property/3d.js";
+     js = [ 
+        "https://cdn.jsdelivr.net/gh/valclassdevelop/valclassDocumentation/property/vanilla-tilt.js",
+        "https://cdn.jsdelivr.net/gh/valclassdevelop/valclassDocumentation/property/tilt.js"
+    ]
+    
+    for(i = 0; i < js.length; i++) {
+        script = document.createElement("script")
+        body = document.getElementsByTagName("body")[0]
+        script.src = js[i];
+        body.appendChild(script)
+     }
 
     script = document.createElement("script")
     body = document.getElementsByTagName("body")[0]
