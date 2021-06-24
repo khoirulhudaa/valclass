d5e82c4db0ece13cc6ce8700449020b1728ed72b
Muhammad Khoirul Huda <79193720+valclassdevelop@users.noreply.github.com>
2021-06-24 23:49:20 +0700

    Update DOMnavbar.js

--- property/DOMnavbar.js
+++ property/DOMnavbar.js
@@ -1,13 +1,13 @@
 if (document.querySelector('.v-navbar')) {
 
     document.querySelector('.v-btn-toggle input').addEventListener('click', () => {
-        document.querySelector('.v-navbar ul').classList.toggle('show')
+        document.querySelector('.v-navbar').classList.toggle('show')
     })
 }
 
 if (document.querySelector('.v-navbar-fixed')) {
 
     document.querySelector('.v-btn-toggle input').addEventListener('click', () => {
-        document.querySelector('.v-navbar-fixed ul').classList.toggle('show')
+        document.querySelector('.v-navbar-fixed').classList.toggle('show')
     })
 }
