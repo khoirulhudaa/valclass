9460ff6aa138880f4a0c9888084e5687a42b836d
Muhammad Khoirul Huda <79193720+valclassdevelop@users.noreply.github.com>
2021-07-10 23:37:40 +0700

    Add files via upload

---
+++ property/currency.js
@@ -0,0 +1,17 @@
+var attr = document.querySelectorAll('.v-currency');
+	var i;
+	for(i = 0; i <= attr.length; i++) {
+		var bilangan = document.querySelectorAll('.v-currency')[i].value;
+		var preceded = document.querySelectorAll('.v-currency')[i].attributes.preceded.value;
+		var number_string = bilangan.replace(/[^,\d]/g, '').toString(),
+		split   		= number_string.split(','),
+		sisa     		= split[0].length % 3,
+		rupiah     		= split[0].substr(0, sisa),
+		ribuan     		= split[0].substr(sisa).match(/\d{3}/gi);
+	 
+        if (ribuan) {
+            separator = sisa ? '.' : '';
+            rupiah += separator + ribuan.join('.');
+            document.querySelectorAll('.v-currency')[i].value = `${preceded}${rupiah}`
+        }
+	}
